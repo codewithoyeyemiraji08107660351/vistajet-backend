c52c81e2263a687604fdccd917dddd10c8785fb6
oyeyemi <babatundeoyeyemi7@gmail.com>
2025-12-01 15:42:58 +0100

    Service commit

--- src/main/java/com/vistajet/vistajet/service/VistajetService.java
+++ src/main/java/com/vistajet/vistajet/service/VistajetService.java
@@ -1,4 +1,18 @@
 package com.vistajet.vistajet.service;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "service_tb")
 public class VistajetService {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 }
