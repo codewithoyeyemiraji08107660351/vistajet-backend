8a4e2b734c0727003de7725da52bba7539ae917c
oyeyemi <babatundeoyeyemi7@gmail.com>
2025-12-05 12:58:34 +0100

    Authentication validation

--- src/main/java/com/vistajet/vistajet/auth/AuthenticationRequest.java
+++ src/main/java/com/vistajet/vistajet/auth/AuthenticationRequest.java
@@ -1,5 +1,8 @@
 package com.vistajet.vistajet.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,6 +12,11 @@ import lombok.Setter;
 @Builder
 public class AuthenticationRequest {
 
+    @Email(message = "Email not valid")
+    @NotBlank(message = "Email cannot be empty")
     private String email;
+
+    @NotBlank(message = "Password cannot be empty")
+    @Size(min = 3)
     private String password;
 }
